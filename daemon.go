819151409856e316b12a50e1b8ea996e074c4328
Xg <xgdapg@gmail.com>
2013-12-16 16:02:13 +0800

    clean code

--- daemon.go
+++ daemon.go
@@ -15,13 +15,13 @@ func daemon() {
 	if ppid == 1 {
 		return
 	}
-	if dppid := os.Getenv("__daemon_ppid"); dppid == strconv.Itoa(ppid) {
+	if os.Getenv("__daemon_ppid") == strconv.Itoa(ppid) {
 		return
 	}
 
 	cmd := exec.Command(os.Args[0])
 	if len(os.Args) > 1 {
-		cmd = exec.Command(os.Args[0], os.Args[1:]...)
+		cmd.Args = append(cmd.Args, os.Args[1:]...)
 	}
 	cmd.Env = append(os.Environ(), "__daemon_ppid="+strconv.Itoa(os.Getpid()))
 	if err := cmd.Start(); err != nil {
